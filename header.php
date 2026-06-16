4ede0231addc669853c46f1e983912ee0fe54a5f
Your Name <your.email@example.com>
2026-06-16 15:33:31 +0530

    Add Google Ads site tag

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18150916122"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'AW-18150916122');
+    </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
